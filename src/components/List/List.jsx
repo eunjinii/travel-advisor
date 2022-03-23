792558ce4aeef9cdbbf029e0501e4db63de204eb
regina <regina@gccompany.co.kr>
2022-03-23 22:39:56 +0900

    [Add] 장소 리스트 UI 껍데기 추가

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  FormControl,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
+
+import useStyles from "./styles";
 
 const List = () => {
-  return <h1>List</h1>;
+  const classes = useStyles();
+  const { type, setType } = useState("restaurants");
+  const { rating, setRating } = useState(0);
+
+  const places = [
+    {
+      name: "Cool place",
+    },
+    {
+      name: "Cool Beer",
+    },
+    {
+      name: "Best hotel",
+    },
+    {
+      name: "yes place",
+    },
+    {
+      name: "nice Beer",
+    },
+    {
+      name: "Best musium",
+    },
+    {
+      name: "Cool place",
+    },
+    {
+      name: "Cool Beer",
+    },
+    {
+      name: "Best hotel",
+    },
+  ];
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants, Hotels & Attractions around you
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants"> Restaurants </MenuItem>
+          <MenuItem value="hotels"> Hotels </MenuItem>
+          <MenuItem value="attractions"> Attractions </MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}> All </MenuItem>
+          <MenuItem value={3}> Above 3.0 </MenuItem>
+          <MenuItem value={4}> Above 4.0 </MenuItem>
+          <MenuItem value={5}> Above 4.5 </MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, idx) => (
+          <Grid item key={idx} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 };
 
 export default List;
